f17b586ccdc0fad82d8404717373772179938e4f
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-12 15:23:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,13 +2,13 @@
 
 
 def map_to_negativize(source_array)
-  result = [] 
-  i = 0 
-  while i < source_array.count do 
-    result << source_array[i] * -1 
-    i += 1 
+  result = []
+  i = 0
+  while i < source_array.count do
+    result << source_array[i] * -1
+    i += 1
   end
-  return result 
+  return result
 end
 
 def map_to_no_change(source_array)
@@ -16,54 +16,54 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  result = [] 
-  i = 0 
-  while i < source_array.count do 
-    result << source_array[i] * 2   
-    i += 1 
+  result = []
+  i = 0
+  while i < source_array.count do
+    result << source_array[i] * 2
+    i += 1
   end
-  return result 
+  return result
 end
 
 def map_to_square(source_array)
-  result = [] 
-  i = 0 
-  while i < source_array.count do 
-    result << source_array[i] ** 2   
-    i += 1 
+  result = []
+  i = 0
+  while i < source_array.count do
+    result << source_array[i] ** 2
+    i += 1
   end
-  return result 
+  return result
 end
 
 def reduce_to_total(source_array, starting_point)
-  total = 0 
-  i = 0 
-  while i < source_array.count do 
-    total += source_array[i]  
+  total = 0
+  i = 0
+  while i < source_array.count do
+    total += source_array[i]
     i += 1
   end
-  return total 
+  return total
 end
 
 def reduce_to_all_true(source_array)
-  result = [] 
-  i = 0 
-  while i < source_array.count do 
+  result = []
+  i = 0
+  while i < source_array.count do
     if source_array[i]
       result << source_array[i]
     end
-    i += 1 
+    i += 1
   end
   return result
-end 
+end
 
 def reduce_to_any_true(source_array)
-  i = 0 
-  while i < source_array.count do 
+  i = 0
+  while i < source_array.count do
     if source_array[i]
-      return true 
+      return true
     end
-    i += 1 
+    i += 1
   end
-  return false 
+  return false
 end 
