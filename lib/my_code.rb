00cd566748799e5cd32b5d1f8c04313e2518e96b
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-12 15:34:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,13 @@
 
 
 def map_to_negativize(source_array)
-  result = []
-  i = 0
-  while i < source_array.count do
-    result << source_array[i] * -1
-    i += 1
+  result = [] 
+  i = 0 
+  while i < source_array.count do 
+    result << source_array[i] * -1 
+    i += 1 
   end
-  return result
+  return result 
 end
 
 def map_to_no_change(source_array)
@@ -15,59 +15,55 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  result = []
-  i = 0
-  while i < source_array.count do
-    result << source_array[i] * 2
-    i += 1
+  result = [] 
+  i = 0 
+  while i < source_array.count do 
+    result << source_array[i] * 2   
+    i += 1 
   end
-  return result
+  return result 
 end
 
 def map_to_square(source_array)
-  result = []
-  i = 0
-  while i < source_array.count do
-    result << source_array[i] ** 2
-    i += 1
+  result = [] 
+  i = 0 
+  while i < source_array.count do 
+    result << source_array[i] ** 2   
+    i += 1 
   end
-  return result
+  return result 
 end
 
 def reduce_to_total(source_array, starting_point=0)
   total = starting_point
-  i = 0
-  while i < source_array.count do
-    total += source_array[i]
+  i = 0 
+  while i < source_array.count do 
+    total += source_array[i]  
     i += 1
   end
-  return total
+  return total 
 end
 
 def reduce_to_all_true(source_array)
-  result = []
-  i = 0
-  while i < source_array.count do
+  result = [] 
+  i = 0 
+  while i < source_array.count do 
     if source_array[i]
       result << source_array[i]
     end
-    i += 1
+    i += 1 
   end
-  if result.count > 0
-    return true
-  else
-    return false
-  end
-end
+  return result.count != 0 
+end 
 
 def reduce_to_any_true(source_array)
-  i = 0
-  has_true = false
-  while i < source_array.count do
+  i = 0 
+  has_true = false 
+  while i < source_array.count do 
     if source_array[i]
-      has_true = true
+      has_true = true 
     end
-    i += 1
+    i += 1 
   end
-  return has_true
+  return has_true 
 end 
